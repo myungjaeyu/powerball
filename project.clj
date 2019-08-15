66e66eb44a90830a8211db89ccf2cc89df1e432d
u4bi <rootcode10@gmail.com>
2019-08-15 13:25:51 +0900

    chore(project.clj): Add http-kit, toucan, postgresql, environ, enlive.

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject powerball "0.1.0-SNAPSHOT"
   :main powerball.core
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [http-kit "2.3.0"]
+                 [toucan "1.12.0"]
+                 [org.postgresql/postgresql "42.2.5"]
+                 [environ "1.1.0"]
+                 [enlive "1.1.6"]]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
