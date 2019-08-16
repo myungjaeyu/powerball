ed784159b47ebc1a25bd30156be7c2941f3d74ae
u4bi <rootcode10@gmail.com>
2019-08-16 10:04:51 +0900

    feat: Add crawler in while loop.

--- src/powerball/app.clj
+++ src/powerball/app.clj
@@ -1,7 +1,23 @@
 (ns powerball.app
   (:require [powerball.crawler.core :refer [get-powerball]]
-            [powerball.db.service :as db]))
+            [powerball.db.service :as db]
+            [powerball.util :refer [get-yesterday-date]]))
+
+(def date (atom {:current "2019-08-15" :last "2013-01-02"}))
+(def flag (atom true))
+(def row (atom 1))
+
+(defn log [] (println @row (str (:current @date))))
 
 (defn app []
-  (-> (get-powerball "2019-08-15" 1)
-      db/add-powerball-many))
+  (while (true? @flag)
+    (let [data (get-powerball (:current @date) @row)]
+      (db/add-powerball-many data)
+      (log)
+      (if (not= (count data) 0)
+        (swap! row (partial + 10))
+        (if (not= (:current @date) (:last @date))
+          ((fn [] 
+             (reset! row 1) 
+             (swap! date assoc :current (get-yesterday-date (:current @date)))))
+          (reset! flag false))))))
